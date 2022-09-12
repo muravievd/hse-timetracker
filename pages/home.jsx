ca16ac9412caefdb66c028f9e7065f6984d3b60c
Dmitry Muraviev <dmitrymuravjev@gmail.com>
2022-09-12 23:37:58 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.js 	modified:   pages/home.jsx 	modified:   pages/home.module.css 	modified:   pages/index.css 	new file:   public/hseanim.json

--- pages/home.jsx
+++ pages/home.jsx
@@ -4,106 +4,11 @@ import Image from 'next/image'
 import { customLoader } from './_app'
 import { useState, useEffect } from 'react'
 import { getScheduleData } from '../lib/services'
+import 'jquery'
+import $ from 'jquery'
+import gsap from 'gsap'
 
-var timetracker_schedule_demodata = {
-    headers: "tbd",
-    classes: [
-        {
-            group: "Б22ДЗ01 (Ветров И. В.)",
-            timetable: {
-                mon: [
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ["11", "10"],
-                        stopTime: ["12", "30"],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: "c 5.09 по 23.10"
-                    },
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ['13', '00'],
-                        stopTime: ['14', '20'],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: ""
-                    },
-                    {
-                        title: "Креативное проектирование (Adobe Illustrator (базовый уровень))",
-                        type: "Лекция",
-                        startTime: ['14', '40'],
-                        stopTime: ['16', '00'],
-                        room: "М.П.261",
-                        host: "Девяткина Мария Вячеславовна",
-                        note: ""
-                    },
-                    {
-                        title: "Креативное проектирование (Adobe Illustrator (базовый уровень))",
-                        type: "Лекция",
-                        startTime: ['16', '20'],
-                        stopTime: ['17', '40'],
-                        room: "М.П.261",
-                        host: "Девяткина Мария Вячеславовна",
-                        note: ""
-                    },
-                ],
-                tue: [
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ["11", "10"],
-                        stopTime: ["12", "30"],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: ""
-                    },
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ['13', '00'],
-                        stopTime: ['14', '20'],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: ""
-                    },
-                    {
-                        title: "Креативное проектирование (Adobe Illustrator (базовый уровень))",
-                        type: "Лекция",
-                        startTime: ['14', '40'],
-                        stopTime: ['16', '00'],
-                        room: "М.П.261",
-                        host: "Девяткина Мария Вячеславовна",
-                        note: ""
-                    },
-                ],
-                wed: [
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ["11", "10"],
-                        stopTime: ["12", "30"],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: ""
-                    },
-                    {
-                        title: "Креативное проектирование (Основы проектирования. Малый фирменный стиль)",
-                        type: "Лекция",
-                        startTime: ['13', '00'],
-                        stopTime: ['14', '20'],
-                        room: "М.П.261",
-                        host: "Ветров Иван Викторович",
-                        note: ""
-                    },
-
-                ],
-                
-            }
-        }
-    ]
-}
+var filterState = 0
 
 function HomeHead(props){
     return(
@@ -119,7 +24,7 @@ function HomeHead(props){
     )
 }
 
-function fillSchedule(data){
+function fillSchedule(data, detailsFunc){
     var timetracker_schedule = data
     console.log(timetracker_schedule)
     if (typeof window !== 'undefined') {
@@ -134,7 +39,7 @@ function fillSchedule(data){
             for(const scheduleDay in classEntity.timetable){
                 if(classEntity.timetable[scheduleDay].length > 0){
                     for(const scheduleEntity of classEntity.timetable[scheduleDay]){
-                        scheduleContainer.push(<HomeContentEntity time={scheduleEntity.time} room={scheduleEntity.room} type={scheduleEntity.type} classTime={scheduleEntity.note} title={scheduleEntity.title} host={scheduleEntity.host}/>)
+                        scheduleContainer.push(<HomeContentEntity onClick={() => displayHCDetails(detailsFunc, {time: scheduleEntity.time, room: scheduleEntity.room, type: scheduleEntity.type, title: scheduleEntity.title, host: scheduleEntity.host})} time={scheduleEntity.time} room={scheduleEntity.room} type={scheduleEntity.type} classTime={scheduleEntity.note} title={scheduleEntity.title} host={scheduleEntity.host}/>)
                     }
                     var classDay
                     switch(scheduleDay){
@@ -185,11 +90,22 @@ function fillSchedule(data){
 
 }
 
-export default function Home(){
+export default function Home(props){
     var [content, setContent] = useState([])
+    var [filters, setFilters] = useState([])
+    var [contentDetails, setContentDetails] = useState([])
     const fillContent = async () => {
         const data = await getScheduleData()
-        setContent(fillSchedule(data))
+        setContent(fillSchedule(data, setContentDetails))
+        var filterData = []
+        for(const filterEntity of data.classes){
+            filterData.push(filterEntity)
+        }
+        setFilters(<HomeFiltersEntity title="Все группы" content={data} />)
+        gsap.fromTo('.IndexPlaceholder', {top: '0%', opacity: '1'}, {top: '-2%', opacity: '0', duration: .5})
+        setTimeout(function(){
+            props.placeholderFunc([])
+        }, 500)
     }
     useEffect(() => {
         fillContent()
@@ -197,20 +113,68 @@ export default function Home(){
     return(
         <div id='HomePage' className={styles.Home}>
             <HomeHead userName="Муравьев Дмитрий Александрович" />
-            <div className={styles.HomeBar}>
+            <div id='HomeBar' className={styles.HomeBar}>
                 <div style={{"opacity": 1}} id='HomeBar_schedule'>Расписание</div>
-                <div id='HomeBar_rooms'>Свободные аудитории</div>
-                <div id='HomeBar_absent'>Отсутствия студентов</div>
+                <div id='HomeBar_rooms'></div>
+                <div id='HomeBar_absent'></div>
             </div>
-            <div className={styles.HomeFilters}>
-                <HomeFiltersEntity title="Все группы" />
+            <div id='HomeFilters' className={styles.HomeFilters}>
+                {filters}
                 <HomeFiltersEntity title="2022/2023" />
                 <HomeFiltersEntity title="Курс 1" />
                 <HomeFiltersEntity title="Бакалавриат" />
                 <HomeFiltersEntity title="Очная" />
             </div>
-            <div className={styles.HomeContent}>
+            <div onScroll={() => hideHead()} id="HomeContent" className={styles.HomeContent}>
                 {content}
+            </div>
+            {contentDetails}
+        </div>
+    )
+}
+
+function hideHead(){
+    var headScroll = $("#HomeContent").scrollTop()
+    if(headScroll > 200){
+        gsap.to("#HomeBar_schedule", {opacity: '0', y: -10, duration: .2})
+        gsap.to("#HomeFilters", {opacity: '0', height: '0px', paddingTop: "0vh", y: -10, duration: .2})
+        gsap.to("#HomeBar", {height: "0px", paddingTop: "0vh", duration: .2})
+        gsap.to("#HomeContent", {height: "90vh", duration: .2})
+    } else {
+        gsap.to("#HomeBar_schedule", {opacity: '1', y: 0, duration: .2})
+        gsap.to("#HomeFilters", {opacity: '1', paddingTop: "3vh", height: 'auto', y: 0, duration: .2})
+        gsap.to("#HomeBar", {height: "auto", paddingTop: "4vh", duration: .2})
+        gsap.to("#HomeContent", {height: "73vh", duration: .2})
+    }
+
+}
+
+function displayHCDetails(detailsFunc, props){
+    detailsFunc(<HomeContentDetails onClick={() => detailsFunc([])} title={props.title} type={props.type} time={props.time} room={props.room} host={props.host}/>)
+}
+
+
+
+function HomeContentDetails(props){
+    return(
+        <div className={styles.HomeContentDetails}>
+            <div className={styles.HCD_Head}>
+                <div onClick={props.onClick} className={styles.HCD_backbtn}>
+                    к расписанию
+                </div>
+                <div className={styles.HCD_HeadContent}>
+                    <div className={styles.HCD_title}>
+                    {props.title} 
+                    </div>
+                    <div className={styles.HCD_HeadDetails}>
+                        <div className={styles.HCD_HD_type}>
+                            {props.type}
+                        </div>
+                        <div className={styles.HCD_HD_time}>{props.time}</div>
+                        <div className={styles.HCD_HD_room}>{props.room}</div>
+                    </div>
+                </div>
+                <div className={styles.HCD_Host}>{props.host}</div>
             </div>
         </div>
     )
@@ -218,7 +182,7 @@ export default function Home(){
 
 function HomeContentEntity(props){
     return(
-        <div className={styles.HomeContentEntity}>
+        <div onClick={props.onClick} className={styles.HomeContentEntity}>
             <div className={styles.HCE_infoContainer}>
                 <div className={styles.HCE_infoContainerEntity}>{props.time}</div>
                 <div className={styles.HCE_infoContainerEntity}>{props.room}</div>
@@ -234,8 +198,34 @@ function HomeContentEntity(props){
 }
 
 function HomeFiltersEntity(props) {
+    var [filterContent, setFilterContent] = useState([])
+    var selector = ["Entry", "Entry", "Entry", "Entry", "Entry"]
+    var selectorContent = []
+    for(const selectorEntity of selector){
+        selectorContent.push(<HomeFiltersSelectorEntity title={selectorEntity} />,)
+    }
+    var filterCompiled = []
+    const displayContents = () => {
+        if(filterState == 0){
+            filterCompiled.push(<div className={styles.HomeFiltersEntity_selector}>{selectorContent}</div>)
+            setFilterContent(filterCompiled)
+            filterState = 1
+        } else {
+            setFilterContent([])
+            filterState = 0
+        }
+    }
     return(
-        <div className={styles.HomeFiltersEntity}>
+        <div onClick={displayContents} className={styles.HomeFiltersEntity}>
+            {props.title}
+            {filterContent}
+        </div>
+    )
+}
+
+function HomeFiltersSelectorEntity(props){
+    return(
+        <div className={styles.HomeFiltersSelectorEntity}>
             {props.title}
         </div>
     )
